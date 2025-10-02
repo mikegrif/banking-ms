5a22a9452c12e237cfdc584fa9bbd7a4e7f6a4ca
mikegrif <mikegrif@bellsouth.net>
2025-10-02 17:57:17 -0400

    added Spring Cloud config server

---
+++ config/src/test/java/com/mikegrif/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikegrif.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
