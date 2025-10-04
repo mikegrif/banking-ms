704b2f6fd6463b0988d2b01493c4e388977dfc6d
mikegrif <mikegrif@bellsouth.net>
2025-10-04 12:52:28 -0400

    Refresh configuration using actuator path

--- cards/src/main/java/com/mikegrif/cards/dto/CardsServiceConfig.java
+++ cards/src/main/java/com/mikegrif/cards/dto/CardsServiceConfig.java
@@ -1,10 +1,19 @@
 package com.mikegrif.cards.dto;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 import java.util.Map;
 
+@Getter
+@Setter
 @ConfigurationProperties(prefix = "cards")
-public record CardsServiceConfig(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+public class CardsServiceConfig {
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 
 }
--- loans/src/main/java/com/mikegrif/loans/dto/LoansServiceConfig.java
+++ loans/src/main/java/com/mikegrif/loans/dto/LoansServiceConfig.java
@@ -1,10 +1,20 @@
 package com.mikegrif.loans.dto;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 import java.util.Map;
 
+@Getter
+@Setter
 @ConfigurationProperties(prefix = "loans")
-public record LoansServiceConfig(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+public class LoansServiceConfig {
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 
 }
+
