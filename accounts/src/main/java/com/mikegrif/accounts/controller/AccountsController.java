c3d683b7b41aeb1df2562edf5be39dfec1050bd8
mikegrif <mikegrif@bellsouth.net>
2025-11-24 11:11:40 -0500

    add rate-limit in account service

--- accounts/src/main/java/com/mikegrif/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/mikegrif/accounts/controller/AccountsController.java
@@ -7,6 +7,7 @@ import com.mikegrif.accounts.dto.ErrorResponseDto;
 import com.mikegrif.accounts.dto.ResponseDto;
 import com.mikegrif.accounts.service.IAccountsService;
 
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -234,11 +235,18 @@ public class AccountsController {
             )
     }
     )
+    @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Java 21");
     }
 
     @Operation(
